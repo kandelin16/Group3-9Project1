e775a063ddd561c19442a479ca4cddbb613513eb
sweetdaniela23 <90408575+sweetdaniela23@users.noreply.github.com>
2022-02-08 10:07:51 -0700

    Startup.cs and the models. Also added a couple things in the home controller to make my database work. The database works and has been seeded. Also installed microsoft entity framework packages. I ran migrations and database update.

--- Group3-9Project/Controllers/HomeController.cs
+++ Group3-9Project/Controllers/HomeController.cs
@@ -12,26 +12,22 @@ namespace Group3_9Project.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
+        private TaskEntryContext teContext { get; set; }
+        public HomeController(TaskEntryContext someName)
         {
-            _logger = logger;
+            teContext = someName;
         }
+
+        //public HomeController(ILogger<HomeController> logger)
+        //{
+        //    _logger = logger;
+        //}
 
         public IActionResult Index()
         {
             return View();
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
     }
 }
--- Group3-9Project/Startup.cs
+++ Group3-9Project/Startup.cs
@@ -1,9 +1,11 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Group3_9Project.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -24,6 +26,10 @@ namespace Group3_9Project
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddDbContext<TaskEntryContext>(options =>
+            {
+                options.UseSqlite(Configuration.GetConnectionString("BlahConnection"));
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
